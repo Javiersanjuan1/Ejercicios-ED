1a99a9673a421c7f80e96799259cfe5ee639b5c7
Javiersanjuan1 <fsanjua@myuax.com>
2024-04-20 23:28:43 +0200

    Initial commit

--- MaximoVectorRecursivo.java
+++ MaximoVectorRecursivo.java
@@ -7,11 +7,9 @@ public class MaximoVectorRecursivo {
     }
 
     public static int encontrarMaximo(int[] vector, int indice) {
-        // Caso base: si el índice alcanza el final del vector, se devuelve el mínimo valor posible
         if (indice == vector.length - 1) {
             return vector[indice];
         }
-        // Llamada recursiva para encontrar el máximo entre el valor actual y el máximo del resto del vector
         int maximoRestante = encontrarMaximo(vector, indice + 1);
         return Math.max(vector[indice], maximoRestante);
     }
